9b3d887828214f3bed17448320a31ac0d5bc5e4f
Durga <mewadadurga83@gmail.com>
2024-04-11 21:25:15 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:clickable_pojects/utils/Components/DynamicList.dart';
 import 'package:flutter/material.dart';
 
 import 'utils/Components/Calculator.dart';
-import 'utils/Components/Chess.dart';
 import 'utils/Components/Map.dart';
 import 'utils/Components/Scroll.dart';
 
 void main() {
-  runApp(Chess(
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Dynamic(),
       ),
   );
 }
--- lib/utils/Components/DynamicList.dart
+++ lib/utils/Components/DynamicList.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+
 class Dynamic extends StatefulWidget {
   const Dynamic({super.key});
 
@@ -11,7 +12,8 @@ class Dynamic extends StatefulWidget {
 class _DynamicState extends State<Dynamic> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.grey.shade100,
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
         backgroundColor: Colors.cyan.shade900,
         leading: Icon(Icons.menu, size: 20, color: Colors.white),
@@ -26,52 +28,86 @@ class _DynamicState extends State<Dynamic> {
           ),
         ),
       ),
-      body:SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-              decoration:BoxDecoration(
-                color: Colors.blue,
-              ) ,
-            )
-          ],
+      body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: List.generate(element.length, (index) => ContainerBar(index:element[index])),
+          ),
         ),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                element.add(element.length);
+              });
+            },
+            child: Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.cyan.shade900,
+              ),
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                element.removeAt(element.length-1);
+              });
+            },
+            child: Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.cyan.shade900,
+              ),
+              child: Icon(
+                Icons.remove,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton:
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-                GestureDetector(
-                  child: Container(
-                          height: 60,
-                          width: 60,
-                          decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.cyan.shade900,
-                          ),
-                    child:Icon(Icons.add,color: Colors.white,size: 20,),
+    );
+  }
+
+  Padding ContainerBar({required index}) {
+    return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 100,
+                width: 380,
+                decoration: BoxDecoration(
+                    color: (index%2==0)?Colors.lightBlue:Colors.lightBlue.shade900,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    '${index + 1}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
                   ),
                 ),
-
-                 SizedBox(width: 10,),
-                 GestureDetector(
-                   child: Container(
-                    height: 60,
-                    width: 60,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.cyan.shade900,
-                    ),
-                    child:Icon(Icons.remove,color: Colors.white,size: 20,),
-                                   ),
-                 ),
-            ],
-          ),
-      );
+              ),
+            );
   }
 }
 
-
+List element=[];
